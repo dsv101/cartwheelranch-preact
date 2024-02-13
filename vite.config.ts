4fa50744c45d7f5ea571e9f69e35a8121500ea0c
David Vandal <dsv101@gmail.com>
2024-02-12 23:23:48 -0500

    initial branding

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	resolve: {
+		alias: {
+		  src: "/src",
+		  "@assets": "/src/assets",
+		  "@components": "/src/components",
+		  "@pages": "/src/pages",
+		},
+	  },
 });
